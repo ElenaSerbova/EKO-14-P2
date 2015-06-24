935be58a77e0dc45159e05fa34ab2cc14c8dca01
extrud <extrud99@gmail.com>
2015-06-25 02:58:22 +0300

    angular homework

---
+++ Fishkov Artem/ang/js/controllers/StudentsController.js
@@ -0,0 +1,57 @@
+		var myApp=angular.module('myApp');
+			myApp.controller("studentsController",function($scope){
+					
+					$scope.students=[{
+						name: "Vasyly",
+						surname:"Ivanov",
+						birthday:
+						{
+							day:12,
+							month:5,
+							year:1990
+						}
+						},
+						{
+						name: "Vasysly",
+						surname:"Ivanov",
+						birthday:
+						{
+							day:12,
+							month:5,
+							year:1990
+						}
+						},
+						{
+						name: "Vasysly",
+						surname:"Ivanov",
+						birthday:
+						{
+							day:12,
+							month:5,
+							year:1990
+						}
+						},
+						{
+						name: "Vasysly",
+						surname:"Ivanov",
+						birthday:
+						{
+							day:12,
+							month:5,
+							year:1990
+						}
+						},
+						{
+						name: "Vasysly",
+						surname:"Ivanov",
+						birthday:
+						{
+							day:12,
+							month:5,
+							year:1990
+						}
+						}
+						]
+					
+					
+			})
